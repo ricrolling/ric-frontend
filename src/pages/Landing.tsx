0e91e9f394b65f2a46148f300d65de0e8d12f54b
chen <23054115+cwkang1998@users.noreply.github.com>
2023-09-02 16:59:06 +0800

    feat: setup landing redirect to dash

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,3 +1,16 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAccount } from 'wagmi';
+
 export const Landing = () => {
+  const { isConnected } = useAccount();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isConnected) {
+      navigate('/rollups');
+    }
+  }, [isConnected]);
+
   return <></>;
 };
